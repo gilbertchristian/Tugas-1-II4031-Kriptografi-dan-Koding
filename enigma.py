0d9d86b287685a59dca71287957317caa386ec9d
natasyazzhr1 <72095268+natasyazzhr1@users.noreply.github.com>
2022-02-03 01:37:22 +0700

    add enigma.py

--- enigma.py
+++ enigma.py
@@ -1,39 +1,70 @@
+from itertools import cycle
+
+slow_list = [21, 3, 15, 1, 19, 10, 14, 26, 20, 8, 16,
+            7, 22, 4, 11, 5, 17, 9, 12, 23, 18, 2, 25, 6, 24, 13]
+medium_list = [20, 1, 6, 4, 15, 3, 14, 12, 23, 5, 16, 2,
+            22, 19, 11, 18, 25, 24, 13, 7, 10, 8, 21, 9, 26, 17] 
+fast_list = [8, 18, 26, 17, 20, 22, 10, 3, 13, 11, 4,
+            23, 5, 24, 9, 12, 25, 16, 19, 6, 15, 21, 2, 7, 1, 4]    
+
 def slow_rotor(pos):
-    slow_list = [21, 3, 15, 1, 19, 10, 14, 26, 20, 8, 16,
-                 7, 22, 4, 11, 5, 17, 9, 12, 23, 18, 2, 25, 6, 24, 13]
     for i in range(len(slow_list)):
-        if i == slow_list[i]:
+        if pos == slow_list[i]:
             return(i)
 
 
 def medium_rotor(pos):
-    medium_list = [20, 1, 6, 4, 15, 3, 14, 12, 23, 5, 16, 2,
-                   22, 19, 11, 18, 25, 24, 13, 7, 10, 8, 21, 9, 26, 17]
     for i in range(len(medium_list)):
-        if i == medium_list[i]:
+        if pos == medium_list[i]:
             return(i)
 
 
 def fast_rotor(pos):
-    fast_list = [8, 18, 26, 17, 20, 22, 10, 3, 13, 11, 4,
-                 23, 5, 24, 9, 12, 25, 16, 19, 6, 15, 21, 2, 7, 1, 4]
     for i in range(len(fast_list)):
-        if i == fast_list[i]:
-            return(fast_list[i])
+        if pos == fast_list[i]:
+            letter_convert = chr(i + 97)
+            return(letter_convert)
 
+def letter(plaintext):
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    for i in range (26):
+        if alphabet[i] == plaintext:
+            num_letter = i
+    return (num_letter)
 
-def cycle(key):
+def search(rotor, key):
     cycle = []
-    alphabet = list('abcdefghiklmnopqrstuvwxyz')
-    print(key)
-    for i in range(26):
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+
+    for i in range (26):
         if alphabet[i] == key:
-            num = i + 1
+            num_key = i + 1
             break
     for i in range(26):
-        cycle.append((i + num) % 26)
-    return(cycle)
+        index = (num_key + i) % 26
+        if index == 0:
+            index = 26
+        cycle.append(index)
+    num_rotor = cycle[rotor]
+    print(cycle)
 
+    return(num_rotor)
 
+def letter_output(rotor3):
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    for i in range (26):
+        if alphabet[i] == rotor3:
+            num_letter = i
+    return (alphabet[num_letter])
+    
 plaintext = input("Enter your plaintext: ").lower()
-print(cycle('z'))
+key1 = input("Enter your key: ").lower()
+rotor1 = letter(plaintext)
+cycle1 = search(rotor1, key1)
+key2 = input("Enter your key: ").lower()
+rotor2 = slow_rotor(cycle1)
+cycle2 = search (rotor2, key2)
+key3 = input("Enter your key: ").lower()
+rotor3 = medium_rotor(cycle2)
+cycle3 = search (rotor3, key3)
+rotor4 = fast_rotor(cycle3)
