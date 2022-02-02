af7c40e8ff3129be5b4f5bafb2a150451860ced8
gilbertchristian <gilbert.spenzha23@gmail.com>
2022-02-02 09:23:46 +0700

    Update playfair.py
    encrypt step 1-2

--- playfair.py
+++ playfair.py
@@ -1,3 +1,6 @@
+import math
+
+
 def create_key(key):
     text = ''.join(dict.fromkeys(key))
     text = ''.join(i for i in key if i.isalnum())
@@ -15,10 +18,30 @@ def create_matrix(row, column, full_key):
         for j in range(column):
             row_list.append(full_key[row * i + j])
         matrix.append(row_list)
-    print(matrix)
+        print(row_list)
 
 
-# plaintext = input("Enter your plaintext: ").lower()
+def abc(plaintext, key):
+    for letter in plaintext:
+        if letter == 'j':
+            plaintext = plaintext.replace('j', 'i')
+
+    # MISAHIN JADI BIGRAM
+    # plaintext = list(plaintext)
+    # num = math.ceil(int(len(plaintext)/2))
+    # bigram_plaintext = []
+    # for i in range(num):
+    #     bigram = []
+    #     for j in range(2):
+    #         bigram.append(plaintext[2 * i + j])
+    #     bigram_plaintext.append(bigram)
+    # print(bigram_plaintext)
+
+    return(plaintext)
+
+
+plaintext = input("Enter your plaintext: ").lower()
 key = input("Enter your key: ").lower()
 full_key = create_key(key)
 create_matrix(5, 5, full_key)
+abc(plaintext, full_key)
