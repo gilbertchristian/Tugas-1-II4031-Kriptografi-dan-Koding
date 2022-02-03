dcfff04ec9eedb307cc8c2d4ed233d2cb9808844
gilbertchristian <18219005@std.stei.itb.ac.id>
2022-02-03 21:27:13 +0700

    Update main.py
    add pathway

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QPixmap
 
+from viginere import *
+
 import sqlite3
 import os
 import csv
@@ -26,21 +28,73 @@ class Menu(QMainWindow):
 
     def Extended(self):
         extended = Extended()
+        widget.addWidget(extended)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def Playfair(self):
         playfair = Playfair()
+        widget.addWidget(playfair)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
     
     def Enigma(self):
         enigma = Enigma()
+        widget.addWidget(enigma)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 class Viginere(QMainWindow):
     def __init__(self):
         super(Viginere, self).__init__()
         loadUi("cipher.ui", self)
+        self.label_6.setText("Viginere Cipher")
+        self.pushButton_5.clicked.connect(self.Menu)
+        self.pushButton.clicked.connect(self.Default)
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
         # plaintext = self.textEdit.text()
         # key = self.textEdit_2.text()
+    
+    def Default(self):
+        default = a()
+        widget.addWidget(default)
+        widget.setCurrentIndex(widget.currentIndex() + 1)    
 
+class Extended(QMainWindow):
+    def __init__(self):
+        super(Extended, self).__init__()
+        loadUi("cipher.ui", self)
+        self.label_6.setText("Ext. Viginere Cipher")
+        self.pushButton_5.clicked.connect(self.Menu)
 
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+class Playfair(QMainWindow):
+    def __init__(self):
+        super(Playfair, self).__init__()
+        loadUi("cipher.ui", self)
+        self.label_6.setText("Playfair Cipher")
+        self.pushButton_5.clicked.connect(self.Menu)
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class Enigma(QMainWindow):
+    def __init__(self):
+        super(Enigma, self).__init__()
+        loadUi("cipher.ui", self)
+        self.label_6.setText("Enigma Cipher")
+        self.pushButton_5.clicked.connect(self.Menu)
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 # main
 app = QApplication(sys.argv)
 welcome = Menu()
