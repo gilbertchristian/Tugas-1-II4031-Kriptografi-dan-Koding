12b29825636ce312b0e360d7695486a2bcab1655
natasyazzhr1 <72095268+natasyazzhr1@users.noreply.github.com>
2022-02-04 11:16:41 +0700

    add decrypt to otp.py

--- otp.py
+++ otp.py
@@ -15,21 +15,33 @@ def encrypt(plaintext):
         final.append(result)
 
     end = "".join(final)
-    print (otp)
-    print (end)
     return otp, end
 
+def decrypt(ciphertext, otp):
+    final = []
+    listcipher = list(ciphertext)
+    listotp = list(otp)
+
+    for i in range(len(ciphertext)):
+        output = (number(listcipher[i]) - number(listotp[i])) % 26
+        result = letter(output)
+        final.append(result)
+
+    end = "".join(final)
+    return end
 
 def number(letter):
-    return(ord(letter) - 96)
+    return(ord(letter) - 97)
 
 def letter(number):
-    return(chr(number + 96))
+    return(chr(number + 97))
 
 plaintext = input("Enter your plaintext: ").lower()
 encrypted = encrypt(plaintext)
+decrypted = decrypt(encrypted[1], encrypted[0])
 
 
 print("OTP: " + encrypted[0])
 print("Encrypted: " + encrypted[1])
+print("Decrypted: " + decrypted)
 
